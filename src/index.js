f11819972f0c08cea5a50c8c69d02d597bde8711
Alexander Shelyugov <62933903+AlexanderShelyugov@users.noreply.github.com>
2020-04-01 23:29:38 +0300

    Add working test

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
-import express from 'express'
-import config from 'config'
-const app = express()
-const port = config.get('port')
-app.listen(port,()=>
-    console.log(`Server is listening on port ${port}`))
+import config from 'config';
+import express from 'express';
+
+const debug = require('debug')('server:debug');
+const app = express();
+const port = config.get('port');
+const name = config.get('name');
+
+const listen = app.listen(port, () => {
+    debug(`server is running on port ${port} and in ${name} mode`);
+    console.log(`server is running on port ${port} and in ${name} mode`);
+})
+
 module.exports = app;
+// module.exports.port = listen.address().port;
